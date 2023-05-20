da815271b4410e569ad6c6f3e722638ec6e12626
designeralabi <bdlkrmalabi@gmail.com>
2023-05-20 20:17:29 +0100

    added more images from adsa_11

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,6 +1,34 @@
 <template>
   <div class="mx-auto container max-w-7xl">
-    <h1 class="text-7xl">Gallery page</h1>
-    
+    <h1 class="text-5xl font-bold flex items-center justify-center mt-24">Gallery page</h1>
+    <div class="flex my-32 gap-2">
+      <div class="w-72 h-96">
+        <img
+          src="../assets/images/adsa_11.jpeg"
+          alt="adsa_11"
+          height="150"
+          width="150"
+          class="rounded-xl h-full w-full object-cover"
+        />
+      </div>
+      <div class="w-[900px] h-96">
+        <img
+          src="../assets/images/adsa_14.jpeg"
+          alt="adsa_11"
+          height="150"
+          width="150"
+          class="rounded-xl h-full w-full object-cover"
+        />
+      </div>
+      <div class="w-72 h-96">
+        <img
+          src="../assets/images/adsa_15.jpeg"
+          alt="adsa_11"
+          height="150"
+          width="150"
+          class="rounded-xl h-full w-full object-cover"
+        />
+      </div>
+    </div>
   </div>
 </template>
