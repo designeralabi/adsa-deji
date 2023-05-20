ab8939f4dde438c476f5eb6c9b7c05563e997a7c
designeralabi <bdlkrmalabi@gmail.com>
2023-05-20 17:58:24 +0100

    styling about layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,36 +1,94 @@
 <template>
   <div class="container mx-auto max-w-7xl">
-    <h3 class="text-5xl my-24 font-bold text-center">Providing access to Entertainment services for all Africans disapora</h3>
-    <div></div>
-    <h2>Our Mission</h2>
-    <p>
-      Our mission is to provide a positive and inclusive environment for squash
-      players of African descent in America, while also giving back to the youth
-      in Africa through the sport. We aim to develop and promote the skills and
-      talents of our team members, while also fostering a sense of community and
-      social responsibility. Through our efforts, we hope to inspire and empower
-      the next generation of squash players in Africa, while also building a
-      strong and successful team in America.
-    </p>
-  </div>
-  <div>
-    <h2>Our Vision</h2>
-    <p>
-      Our vision is to create a community of passionate squash players in
-      America that celebrates and promotes the diversity of the African diaspora
-      through the sport. We strive to empower and inspire individuals to reach
-      their full potential on and off the court, while fostering a culture of
-      inclusivity and excellence.
-    </p>
-  </div>
-  <div>
-    <h2>Our Goals</h2>
+    <h3
+      class="text-5xl my-24 leading-normal flex items-center justify-center font-bold"
+    >
+      <span class="w-8/12 block text-center">
+        Providing access to Entertainment services for all Africans disapora
+      </span>
+    </h3>
+    <div class="flex items-center gap-8 mt-40">
+      <div class="w-72 h-72 -translate-y-32">
+        <img
+          src="../assets//images/adsa_1.jpeg"
+          alt="adsa_1"
+          height="150"
+          width="150"
+          class="rounded-3xl h-full w-full object-cover"
+        />
+      </div>
+      <div class="w-72 h-96">
+        <img
+          src="../assets//images/adsa_2.jpg"
+          alt="adsa_1"
+          height="150"
+          width="150"
+          class="rounded-3xl h-full w-full object-cover"
+        />
+      </div>
+      <div class="w-[400px] h-96">
+        <img
+          src="../assets//images/adsa_3.jpg"
+          alt="adsa_1"
+          height="150"
+          width="150"
+          class="rounded-3xl h-full w-full object-cover"
+        />
+      </div>
+      <div class="w-52 h-64 -translate-y-32">
+        <img
+          src="../assets//images/adsa_4.jpg"
+          alt="adsa_1"
+          height="150"
+          width="150"
+          class="rounded-3xl h-full w-full object-cover"
+        />
+      </div>
+      <div class="w-72 h-96">
+        <img
+          src="../assets//images/adsa_5.jpg"
+          alt="adsa_1"
+          height="150"
+          width="150"
+          class="rounded-3xl h-full w-full object-cover"
+        />
+      </div>
+    </div>
+    <!-- mission -->
+    <div class="bg-[#747bff] my-48 p-8">
+      <h2 class="text-white text-3xl font-bold">Our Mission</h2>
+      <p class=" text-white my-8 text-xl leading-normal">
+        Our mission is to provide a positive and inclusive environment for
+        squash players of African descent in America, while also giving back to
+        the youth in Africa through the sport. We aim to develop and promote the
+        skills and talents of our team members, while also fostering a sense of
+        community and social responsibility. Through our efforts, we hope to
+        inspire and empower the next generation of squash players in Africa,
+        while also building a strong and successful team in America.
+      </p>
+    </div>
+    <!-- vision -->
     <div>
-      <h3>
-        Providing access to squash equipment and training for young people in
-        underprivileged communities.
-      </h3>
-      h
+      <h2>Our Vision</h2>
+      <p>
+        Our vision is to create a community of passionate squash players in
+        America that celebrates and promotes the diversity of the African
+        diaspora through the sport. We strive to empower and inspire individuals
+        to reach their full potential on and off the court, while fostering a
+        culture of inclusivity and excellence.
+      </p>
+    </div>
+
+    <!-- goals -->
+    <div>
+      <h2>Our Goals</h2>
+      <div>
+        <h3>
+          Providing access to squash equipment and training for young people in
+          underprivileged communities.
+        </h3>
+        h
+      </div>
     </div>
   </div>
 </template>
