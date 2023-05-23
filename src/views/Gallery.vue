e2c36541a0eddae239913875aa9d613d376e4042
designeralabi <bdlkrmalabi@gmail.com>
2023-05-23 23:19:49 +0100

    added upcoming  and past image to the homePage

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -3,7 +3,7 @@
     <h1 class="text-5xl font-bold flex items-center justify-center mt-24">
       Gallery page
     </h1>
-    <div class="flex flex-col lg: flex-row my-32 gap-2">
+    <div class="flex flex-wrap flex-col lg:flex-row my-32 gap-2">
       <div class="w-72 h-96">
         <img
           src="../assets/images/adsa_11.jpeg"
@@ -22,9 +22,54 @@
           class="rounded-xl h-full w-full object-cover"
         />
       </div>
+      <div class="w-[900px] h-96">
+        <img
+          src="../assets/images/adsa_19.jpeg"
+          alt="adsa_11"
+          height="150"
+          width="150"
+          class="rounded-xl h-full w-full object-cover"
+        />
+      </div>
       <div class="w-72 h-96">
         <img
           src="../assets/images/adsa_15.jpeg"
+          alt="adsa_11"
+          height="150"
+          width="150"
+          class="rounded-xl h-full w-full object-cover"
+        />
+      </div>
+      <div class="w-72 h-96">
+        <img
+          src="../assets/images/adsa_1.jpeg"
+          alt="adsa_11"
+          height="150"
+          width="150"
+          class="rounded-xl h-full w-full object-cover object-right-bottom"
+        />
+      </div>
+      <div class="w-[400px] h-96">
+        <img
+          src="../assets/images/adsa_16.jpeg"
+          alt="adsa_11"
+          height="150"
+          width="150"
+          class="rounded-xl h-full w-full object-cover"
+        />
+      </div>
+      <div class="w-72 h-96">
+        <img
+          src="../assets/images/adsa_15.jpeg"
+          alt="adsa_11"
+          height="150"
+          width="150"
+          class="rounded-xl h-full w-full object-cover"
+        />
+      </div>
+      <div class="w-48 h-96">
+        <img
+          src="../assets/images/adsa_10.jpeg"
           alt="adsa_11"
           height="150"
           width="150"
